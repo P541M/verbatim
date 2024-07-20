b1e809bbbc6eaa26aab90310d676df2eeb8c4482
P541M <videna.psalmeleazar@gmail.com>
2024-07-20 13:54:12 -0400

    feat: working RAHHH

--- src/components/QuoteList.jsx
+++ src/components/QuoteList.jsx
@@ -1,15 +1,41 @@
-import React, { useState } from "react";
-import { quotes as initialQuotes } from "../data/quotes";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import QuoteCard from "./QuoteCard";
 
+const getDeviceId = () => {
+  let deviceId = localStorage.getItem("deviceId");
+  if (!deviceId) {
+    deviceId = "_" + Math.random().toString(36).substr(2, 9);
+    localStorage.setItem("deviceId", deviceId);
+  }
+  return deviceId;
+};
+
 const QuoteList = () => {
-  const [quotes, setQuotes] = useState(initialQuotes);
+  const [quotes, setQuotes] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/quotes")
+      .then((response) => setQuotes(response.data))
+      .catch((error) => console.error("Error fetching quotes:", error));
+  }, []);
 
   const handleLike = (id) => {
-    const updatedQuotes = quotes.map((quote) =>
-      quote.id === id ? { ...quote, likes: quote.likes + 1 } : quote
-    );
-    setQuotes(updatedQuotes);
+    const deviceId = getDeviceId();
+    axios
+      .post(`http://localhost:5000/quotes/${id}/like`, { deviceId })
+      .then((response) => {
+        if (response.data.success) {
+          const updatedQuotes = quotes.map((quote) =>
+            quote.id === id ? { ...quote, likes: quote.likes + 1 } : quote
+          );
+          setQuotes(updatedQuotes);
+        } else {
+          alert(response.data.message);
+        }
+      })
+      .catch((error) => console.error("Error liking quote:", error));
   };
 
   return (
